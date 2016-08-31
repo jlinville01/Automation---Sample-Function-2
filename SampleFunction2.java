52c8ce169c0bdaef28c6ad1f508b310417b1c73c
Justin Linville <jlinville01@gmail.com>
2016-08-31 12:22:08 -0700

    Automation Sample Function 2
    This function was used to pull a users Token ID from the database

---
+++ SampleFunction2.java
@@ -0,0 +1,14 @@
+public String getTokenID(String email) throws Exception
+{
+	stmt = con.createStatement();
+	String runReset = "SELECT UDF_GetTokenIdForEmail('" + email + "') as TokenId;";
+	ResultSet result = stmt.executeQuery(runReset);
+	String tokenId = null;
+
+	while (result.next())
+	{
+		tokenId = result.getString("TokenId"); 
+	}
+	
+	return tokenId;
+}
